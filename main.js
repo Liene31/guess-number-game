5022407662f735c9c473f0dd01ea7ca79fa6304f
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-09-28 15:31:44 +0200

    update game functionality

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const gameLogicContainer = document.getElementById("game-logic-container");
 
 const tentativePara = document.getElementById("tentative-message");
 const messageResult = document.getElementById("message-result");
+const characterImage = document.getElementById("character-img");
 
 const numberInput = document.getElementById("guess-input");
 const guessBtn = document.getElementById("guess-btn");
+const newGameBtn = document.getElementById("new-game-btn");
 
 let tentative;
 let maxNumber;
@@ -15,9 +17,36 @@ let randomNumber;
 document.getElementById("lvl-easy").addEventListener("click", () => {
   landingPageContainer.style.display = "none";
   gameLogicContainer.style.display = "block";
+  characterImage.src = "./images/dwarf.png";
 
   tentative = 3;
   maxNumber = 10;
+  randomNumber = Math.floor(Math.random() * maxNumber + 1);
+  console.log(randomNumber);
+  tentativePara.textContent = `You have ${tentative} tentative`;
+});
+
+// If level medium pressed landing page disappears and game logic opens
+document.getElementById("lvl-medium").addEventListener("click", () => {
+  landingPageContainer.style.display = "none";
+  gameLogicContainer.style.display = "block";
+  characterImage.src = "./images/elf.png";
+
+  tentative = 10;
+  maxNumber = 100;
+  randomNumber = Math.floor(Math.random() * maxNumber + 1);
+  console.log(randomNumber);
+  tentativePara.textContent = `You have ${tentative} tentative`;
+});
+
+// If level difficult pressed landing page disappears and game logic opens
+document.getElementById("lvl-difficult").addEventListener("click", () => {
+  landingPageContainer.style.display = "none";
+  gameLogicContainer.style.display = "block";
+  characterImage.src = "./images/halfling.png";
+
+  tentative = 15;
+  maxNumber = 1000;
   randomNumber = Math.floor(Math.random() * maxNumber + 1);
   console.log(randomNumber);
   tentativePara.textContent = `You have ${tentative} tentative`;
@@ -33,7 +62,7 @@ guessBtn.addEventListener("click", () => {
     messageResult.textContent = "Bingo";
     messageResult.textContent = "You Won";
     guessBtn.disabled = true;
-    restartBtn.style.display = "block";
+    newGameBtn.style.display = "block";
   } else if (chosenNumber > randomNumber) {
     messageResult.textContent = "It's too big";
     tentative--;
@@ -47,6 +76,11 @@ guessBtn.addEventListener("click", () => {
   if (tentative === 0) {
     messageResult.textContent = "Game Over";
     guessBtn.disabled = true;
-    restartBtn.style.display = "block";
+    newGameBtn.style.display = "block";
   }
+});
+
+//Button Restart the game
+newGameBtn.addEventListener("click", () => {
+  location.reload();
 });
