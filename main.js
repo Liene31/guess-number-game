c576956949864e4fab541ae08e56a55764dc13b9
Liene <l.bare@interface3.be>
2025-10-01 11:24:24 +0200

    refactor the code

--- main.js
+++ main.js
@@ -15,41 +15,17 @@ let randomNumber;
 
 // If level easy pressed landing page disappears and game logic opens
 document.getElementById("lvl-easy").addEventListener("click", () => {
-  landingPageContainer.style.display = "none";
-  gameLogicContainer.style.display = "flex";
-  characterImage.src = "./images/dwarf.png";
-
-  tentative = 3;
-  maxNumber = 10;
-  randomNumber = Math.floor(Math.random() * maxNumber + 1);
-  console.log(randomNumber);
-  tentativePara.textContent = ` ${"".padEnd(tentative * 2, "💀")} `;
+  initGame("easy");
 });
 
 // If level medium pressed landing page disappears and game logic opens
 document.getElementById("lvl-medium").addEventListener("click", () => {
-  landingPageContainer.style.display = "none";
-  gameLogicContainer.style.display = "flex";
-  characterImage.src = "./images/elf.png";
-
-  tentative = 10;
-  maxNumber = 100;
-  randomNumber = Math.floor(Math.random() * maxNumber + 1);
-  console.log(randomNumber);
-  tentativePara.textContent = ` ${"".padEnd(tentative * 2, "💀")} `;
+  initGame("medium");
 });
 
 // If level difficult pressed landing page disappears and game logic opens
 document.getElementById("lvl-difficult").addEventListener("click", () => {
-  landingPageContainer.style.display = "none";
-  gameLogicContainer.style.display = "flex";
-  characterImage.src = "./images/halfling.png";
-
-  tentative = 15;
-  maxNumber = 1000;
-  randomNumber = Math.floor(Math.random() * maxNumber + 1);
-  console.log(randomNumber);
-  tentativePara.textContent = ` ${"".padEnd(tentative * 2, "💀")} `;
+  initGame("difficult");
 });
 
 // Button Guess
@@ -99,3 +75,42 @@ numberInput.addEventListener("keypress", (e) => {
 newGameBtn.addEventListener("click", () => {
   location.reload();
 });
+
+// With a below comment we give an extra information of each function
+//It's functionality and type it expects
+//When calling the function later, it shows that information
+
+/**
+ * Generate a random value
+ * @returns {number}
+ */
+function generateNumber() {
+  return Math.floor(Math.random() * maxNumber + 1);
+}
+
+/**
+ * Initialize game
+ * @param {string} level
+ */
+function initGame(level) {
+  landingPageContainer.style.display = "none";
+  gameLogicContainer.style.display = "flex";
+
+  if (level === "easy") {
+    characterImage.src = "./images/dwarf.png";
+    tentative = 3;
+    maxNumber = 10;
+  } else if (level === "medium") {
+    characterImage.src = "./images/elf.png";
+    tentative = 10;
+    maxNumber = 100;
+  } else {
+    characterImage.src = "./images/halfling.png";
+    tentative = 15;
+    maxNumber = 1000;
+  }
+
+  randomNumber = generateNumber();
+  console.log(randomNumber);
+  tentativePara.textContent = ` ${"".padEnd(tentative * 2, "💀")} `;
+}
